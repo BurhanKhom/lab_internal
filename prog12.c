21f1ef2c8b9b6f4ace328a8047d20d064acfc1b4
Burhanuddin Khomosi <burhan.khomosi@gmail.com>
2017-12-05 07:51:18 +0530

    Update prog12.c

--- prog12.c
+++ prog12.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 #define MAX 20
-int ht[MAX]={0}, f[MAX], i=0;
+int ht[MAX]={0}, f;
 void linear(int k, int key)
 {
-	int l = k%MAX;
-	if(ht[l]==0)
-		ht[l]=key;
+	if(ht[k%MAX]==0)
+		ht[k%MAX]=key;
 	else
 		linear(k+1, key);
 }
 int main()
 {
 	FILE *fp = fopen("data.txt", "r");
-	while(fscanf(fp, "%d", &f[i]) != EOF)
-		linear(f[i], f[i++]);
-	for(i=0; i<MAX; i++)
+	while(fscanf(fp, "%d", &f) != EOF)
+		linear(f, f);
+	for(int i=0; i<MAX; i++)
 		printf("H[%d] = %d\n", i, ht[i]);
 }
